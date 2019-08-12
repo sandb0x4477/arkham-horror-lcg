b5cb027d59aad81c7a18f572411f2eb991f74172
sandb0x4477 <novasettler@gmail.com>
2019-08-13 05:33:41 +1000

    Initial

---
+++ ah-lcg-hapi/src/models/card.model.js
@@ -0,0 +1,125 @@
+const mongoose = require('mongoose');
+
+const CardSchema = mongoose.Schema({
+	pack_code: {
+		type: 'String'
+	},
+	pack_name: {
+		type: 'String'
+	},
+	type_code: {
+		type: 'String'
+	},
+	type_name: {
+		type: 'String'
+	},
+	faction_code: {
+		type: 'String'
+	},
+	faction_name: {
+		type: 'String'
+	},
+	encounter_code: {
+		type: 'String'
+	},
+	encounter_name: {
+		type: 'String'
+	},
+	position: {
+		type: 'Number'
+	},
+	exceptional: {
+		type: 'Boolean'
+	},
+	encounter_position: {
+		type: 'Number'
+	},
+	code: {
+		type: 'String'
+	},
+	name: {
+		type: 'String'
+	},
+	real_name: {
+		type: 'String'
+	},
+	text: {
+		type: 'String'
+	},
+	real_text: {
+		type: 'String'
+	},
+	quantity: {
+		type: 'Number'
+	},
+	clues_fixed: {
+		type: 'Boolean'
+	},
+	health: {
+		type: 'Number'
+	},
+	health_per_investigator: {
+		type: 'Boolean'
+	},
+	enemy_damage: {
+		type: 'Number'
+	},
+	enemy_horror: {
+		type: 'Number'
+	},
+	enemy_fight: {
+		type: 'Number'
+	},
+	enemy_evade: {
+		type: 'Number'
+	},
+	victory: {
+		type: 'Number'
+	},
+	traits: {
+		type: 'String'
+	},
+	real_traits: {
+		type: 'String'
+	},
+	flavor: {
+		type: 'String'
+	},
+	illustrator: {
+		type: 'String'
+	},
+	is_unique: {
+		type: 'Boolean'
+	},
+	exile: {
+		type: 'Boolean'
+	},
+	hidden: {
+		type: 'Boolean'
+	},
+	permanent: {
+		type: 'Boolean'
+	},
+	double_sided: {
+		type: 'Boolean'
+	},
+	octgn_id: {
+		type: 'String'
+	},
+	url: {
+		type: 'String'
+	},
+	imagesrc: {
+		type: 'String'
+	},
+	spoiler: {
+		type: 'Number'
+	},
+	backimagesrc: {
+		type: 'String'
+	}
+}, {
+    timestamps: true
+  });
+
+module.exports = mongoose.model('Card', CardSchema);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '',
+  styles: ['']
 })
-export class AppComponent {
-  title = 'arkham-horror-lcg';
-}
+export class AppComponent {}
